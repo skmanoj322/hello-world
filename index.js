ffa30ef6dff6c6ae963deba84587640be46c6ed6
Manoj sk <manojsk@Mac.lan>
2025-09-25 03:11:26 +0530

    sadkas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World From docker')
+  res.send('Hello World From dockerzz')
 })
 
 app.listen(port, () => {
