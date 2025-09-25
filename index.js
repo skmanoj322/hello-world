f0bce26fee9744515cb1c54c598fe83043def567
Manoj sk <manojsk@Manojs-MacBook-Pro.local>
2025-09-25 13:02:32 +0530

    deployment2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World From dockerzz')
+  res.send('nice its working lets go')
 })
 
 app.listen(port, () => {
