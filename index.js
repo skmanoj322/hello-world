38be2c2819221357de8ff219fb758cebde3004f5
Manoj sk <manojsk@Manojs-MacBook-Pro.local>
2025-09-26 19:44:49 +0530

    docker:test123

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('checking my docker test')
+  res.send('lets goo then how are you azure??')
 })
 
 app.listen(port, () => {
